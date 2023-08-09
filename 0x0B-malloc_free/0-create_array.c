42118d1c73d10a1536c8a32e74216a81f3ef6ee5
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-08-08 19:13:39 -0700

    memory allocation

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ *create_array - Creates array of characters
+ *initialise with specific char
+ *@size: size of an array
+ *@c: char to initialise
+ *Return: Null or pointer
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *b;
+	unsigned int i;
+
+	b = malloc(sizeof(char) * size);
+
+	if (!size || b == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		b[i] = c;
+	}
+	return (b);
+
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ *_strdup - Returns pointer to newly allocated space
+ *contains coppy of string given as parameter
+ *@str: string to be copied
+ *Return: NULL or pointer
+ */
+
+char *_strdup(char *str)
+{
+	int len	= 0;
+	char *r;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (*str++)
+	{
+		len++;
+	}
+	r = malloc(sizeof(char) * (len + 1));
+	if (!r)
+	{
+		return (NULL);
+	}
+	for (len++; len--;)
+	{
+		r[len] = *--str;
+	}
+	return (r);`
+}
