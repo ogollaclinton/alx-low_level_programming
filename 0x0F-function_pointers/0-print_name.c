39b447ee3ca0f9597c7c540015b913345d38b9aa
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-08-16 18:38:40 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+include "function_pointers.h"
+/**
+ * print_name - Print a name
+ * @name: name to printed
+ * @f: A pointer to function that prints a name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+include "function_pointers.h"
+/**
+ * int_index - Search for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: Pointer to function to compare
+ * Return: 0 or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
