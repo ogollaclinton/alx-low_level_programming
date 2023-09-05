7f2b890c375170a1adef3482e2ca577b898e4fc0
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-09-04 19:47:42 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	t = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (z == -1)
+	if (t == -1)
 		return (-1);
 
 	if (text_content != NULL)
