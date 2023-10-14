4b438ac428ced31f3b96bd623b2e4c96493e9371
Clinton Ogolla <clintonmcogolla@gmail.com>
2023-10-14 01:29:01 +0000

    doubly linked

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 
 /**
- *struct dlistint_s - doubly linked list
+ * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
+ * 
  * Description: doubly linked list node structure
  * for Holberton project
  */
